038b79f8224288cb75bfecfab8409cb05fc8b1d5
github username <github email address>
2019-06-12 23:12:29 -0400

    add method enterPass and eventhandler for Keypad component

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
-// Code Keypad Component Here
+import React from "react";
+
+export default class Keypad extends React.Component {
+
+  enterPass = () => {
+    console.log('Enter password...')
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="password" onKeyUp={this.enterPass}></input>
+      </div>
+    )
+  }
+}
