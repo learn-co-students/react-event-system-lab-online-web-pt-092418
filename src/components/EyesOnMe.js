2fcf69fe452d82e1cffb5014ffbd917127bb9697
github username <github email address>
2019-07-30 02:34:24 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
-// Code EyesOnMe Component Here
+import React, {Component} from 'react'
+
+export default class EyesOnMe extends Component {
+
+  whenfocused = () => {
+    console.log('Good!')
+  };
+  whenblur = () => {
+    console.log('Hey! Eyes on me!')
+  };
+
+  render() {
+    return(
+      <div>
+        <form>
+          <button onFocus={this.whenfocused} onBlur={this.whenblur}/>
+        </form>
+      </div>
+    )
+  }
+}// Code EyesOnMe Component Here
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,23 @@
-// Code Keypad Component Here
+import React, {Component} from 'react'
+
+export default class Keypad extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: " "
+    }
+  }
+
+
+  typing = () => {
+    console.log('Entering password...')
+  };
+
+  render() {
+    return (
+      <form>
+        <input type="password" onKeyUp={this.typing}/>
+      </form>
+    );
+  }
+}// Code Keypad Component Here
